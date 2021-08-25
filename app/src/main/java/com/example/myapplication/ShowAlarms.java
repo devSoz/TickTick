3ecc1daa455614ebb7743c0916ec2de1512f9d68
Devipriya Sozharajan <devipriyasabi@gmail.com>
2021-08-25 07:31:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ShowAlarms.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickDateTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_date_time);
+    }
+}
