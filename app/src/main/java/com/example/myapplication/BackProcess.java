e613e4f2c889eae78b6408e6d961818cffcbdeba
Devipriya Sozharajan <devipriyasabi@gmail.com>
2021-08-26 12:51:27 +0530

    One timer with no controls

--- app/src/main/java/com/example/myapplication/BackProcess.java
+++ app/src/main/java/com/example/myapplication/BackProcess.java
@@ -2,13 +2,16 @@ package com.example.myapplication;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.Build;
 
 
 public class BackProcess extends Application {
     public static BackProcess instance;
     @Override
     public void onCreate() {
-        startService(new Intent(this,AlarmService.class));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(new Intent(this,AlarmService.class));// startService(new Intent(this,AlarmService.class));
+        }
         instance = this;
         super.onCreate();
     }
